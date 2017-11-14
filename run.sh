eb651cb79abe5e9236643d822663b6db8150bf55
Ramon Henrique <rhpr@cin.ufpe.br>
2017-11-14 16:31:36 -0300

    Redirect output to /dev/null file

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export `dbus-launch`
 mongod &
-/etc/knot/start.sh knot-fog 3&>1
-/etc/knot/start.sh knot-web 3&>1
+/etc/knot/start.sh knot-fog 3&> /dev/null
+/etc/knot/start.sh knot-web 3&> /dev/null
 node server.js &
 /bin/bash
 #run node client.js
